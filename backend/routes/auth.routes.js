615eb0a9010b8bf33b8e863215fc2f8f7e11746b
myselfpoulomi <routhpoulomi@gmail.com>
2025-03-07 17:24:47 +0530

    signup login and logout routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,26 +1,14 @@
 import express from "express";
+import {signup} from "../controllers/auth.controller.js";
+import { login } from "../controllers/auth.controller.js";
+import { logout } from "../controllers/auth.controller.js";
 
 const router = express.Router()
 
 
-router.get("/signup", (req, res)=> {
-    res.json({
-        data:"You hit the signup endpoint"
-    })
-}
-)
+router.get("/signup", signup)
 
-router.get("/login", (req, res)=> {
-    res.json({
-        data:"You hit the login endpoint"
-    })
-}
-)
+router.get("/login", login)
 
-router.get("/logout", (req, res)=> {
-    res.json({
-        data:"You hit the logout endpoint"
-    })
-}
-)
+router.get("/logout",logout)
 export default router;
